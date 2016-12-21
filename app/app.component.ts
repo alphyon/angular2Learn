c5eeae0da7428d7c2b8b1fa9a1e53c03360a1df3
José Antonio Henríquez Chavarría <alphyon21@yahoo.es>
2016-12-21 11:47:30 -0600

    prueba ok

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My First Angular App2</h1>',
+  styles:['h1{color:red;border: 1px solid #ee45ff}']
 })
 export class AppComponent { }
